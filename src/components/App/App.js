28f4c17a7d0f95992ce509b43e761936e1212bc7
Ben Zeng <benzeng94@me.com>
2019-10-31 12:14:19 +0000

    imported components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import './App.css';
+import Nav from '../Nav/Nav';
+import Login from '../Login/Login';
+import SignUp from '../SignUp/SignUp';
+import Profile from '../Profile/Profile';
+
 
 function App() {
   return (
