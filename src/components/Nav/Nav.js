9c1d874e1c7a261edec09b6ff2cb361b0c047b26
Ben Zeng <benzeng94@me.com>
2019-10-31 12:57:28 +0000

    added nav link style

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,16 +1,26 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 
 function Nav() {
-  return (
-   <nav>
-       <h3>Logo</h3>
-       <ul className="nav-links">
-           <li>Sign Up</li>
-           <li>Login</li>
-           <li>Log Out</li>
-       </ul>
-   </nav>
-  );
+
+    const navLinkStyle = {
+        color: 'white'
+    };
+
+    return (
+        <nav>
+            <h3>Logo</h3>
+            <ul className="nav-links">
+                <Link style={navLinkStyle} to="/signup">
+                    <li>Sign Up</li>
+                </Link>
+                <Link style={navLinkStyle} to="/login">
+                    <li>Login</li>
+                </Link>
+                <li>Log Out</li>
+            </ul>
+        </nav>
+    );
 }
 
 export default Nav;
