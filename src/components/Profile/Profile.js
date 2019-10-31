2f1a75e7c5d04d69d1536672f238cea2b0082e4b
Ben Zeng <benzeng94@me.com>
2019-10-31 17:50:38 +0000

    added bootstrap react

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 
+
 const profileAPI = "https://api-wagster.herokuapp.com/api/v1/profiles/1";
 // const profileAPI = "http://localhost:3000/api/v1/profiles/1";
 
