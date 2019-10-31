da02ab55e43b03bdc9186e5305286bfc21483b45
Ben Zeng <benzeng94@me.com>
2019-10-31 12:18:36 +0000

    Added nav links

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 function Nav() {
   return (
-    <div>
-        Profile Page!
-    </div>
+   <nav>
+       <h3>Logo</h3>
+       <ul className="Nav-Links">
+           <li>Sign Up</li>
+           <li>Login</li>
+           <li>Log Out</li>
+       </ul>
+   </nav>
   );
 }
 
