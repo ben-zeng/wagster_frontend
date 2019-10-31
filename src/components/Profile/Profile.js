2bbe40c8dd05bd367b565bf3b474993993b83611
Ben Zeng <benzeng94@me.com>
2019-10-31 17:47:05 +0000

    successful api fetch to console

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+
+const profileAPI = "https://api-wagster.herokuapp.com/api/v1/profiles/1";
+// const profileAPI = "http://localhost:3000/api/v1/profiles/1";
 
 function Profile() {
+
+    useEffect(() => {
+        fetchProfile();
+    },[]);
+
+    const fetchProfile = async () => {
+        const data = await fetch (profileAPI).then(response => response.json());
+        console.log(data);
+    };
+
   return (
     <div>
         Profile Page!
