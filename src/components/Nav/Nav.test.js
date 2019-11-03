9820f0599320fbdbe7eed54fc621e6505fae0f96
Ben Zeng <benzeng94@me.com>
2019-11-03 19:10:25 +0000

    stubbed useHistory

--- src/components/Nav/Nav.test.js
+++ src/components/Nav/Nav.test.js
@@ -2,6 +2,14 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Nav from './Nav';
 
+
+jest.mock('react-router-dom', () => ({
+    useHistory: () => ({
+        push: jest.fn(),
+    }),
+}));
+
+
 describe ('Login',() => {
     let wrapper;
     beforeEach(() => wrapper = shallow(<Nav />));
