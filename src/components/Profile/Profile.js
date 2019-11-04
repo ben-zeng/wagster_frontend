04dea95cec5efc21294bc6b60c4d6999af59de82
ilimeza <ilianameza530@gmail.com>
2019-11-04 19:46:56 +0000

    updating profile card

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,65 +1,71 @@
 import React, {useEffect, useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
 import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import Avatar from '@material-ui/core/Avatar';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
+// import Collapse from '@material-ui/core/Collapse';
 import Button from '@material-ui/core/Button';
+// import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
+import { yellow } from '@material-ui/core/colors';
+// import FavoriteIcon from '@material-ui/icons/Favorite';
+// import ShareIcon from '@material-ui/icons/Share';
+// import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+// import MoreVertIcon from '@material-ui/icons/MoreVert';
 // import {Link,useHistory} from 'react-router-dom'
 import Axios from "axios";
 import { resolveAPIEndpoint } from '../../helpers/APIResolveHelper';
 
-    const useStyles = makeStyles({
-        card: {
-          maxWidth: 345,
-        },
-        media: {
-          height: 140,
-        },
-    });
+    // const useStyles = makeStyles({
+    //     card: {
+    //       maxWidth: 345,
+    //     },
+    //     media: {
+    //       height: 140,
+    //     },
+    // });
+
+    const useStyles = makeStyles(theme => ({
+      card: {
+        maxWidth: 345,
+        'margin-left': '30%',
+        justifyContent:'center',
+        width: "90%",
+      },
+      media: {
+        height: 0,
+        paddingTop: '56.25%', // 16:9
+      },
+      expand: {
+        transform: 'rotate(0deg)',
+        marginLeft: 'auto',
+        transition: theme.transitions.create('transform', {
+          duration: theme.transitions.duration.shortest,
+        }),
+      },
+      expandOpen: {
+        transform: 'rotate(180deg)',
+      },
+      avatar: {
+        backgroundColor: yellow[300],
+      },
+    }));
+    
 export default function Profile() {
 
     const [data, setData]  = useState(null);
 
     const classes = useStyles();
+    const [expanded, setExpanded] = React.useState(false);
+    const handleExpandClick = () => {
+      setExpanded(!expanded);
+    };
 
-    // let dataStore = undefined;
-
-
-    //
-    // useEffect(() => {
-    //     Axios.get(resolveAPIEndpoint("profiles/1") ).then(result =>{
-    //         dataStore = result.data;
-    //         console.log(dataStore.dog_name);
-    //
-    //
-    //
-    //     }).catch(error => {
-    //         alert(error)
-    //     })
-    // },  []);
-
-
-    // useEffect(async () => {
-    //     const result = await Axios(resolveAPIEndpoint("profiles/1"));
-    //     // dataStore = result;
-    //     console.log(result.data.dog_name);
-    //     setData(result.data);
-    // },[]);
-
-    // useEffect(() => {
-    //
-    //     const fetchData = async () => {
-    //         const result = await Axios(resolveAPIEndpoint("profiles/1"),
-    //         );
-    //         setData(result.data);
-    //     };
-    //
-    //     fetchData();
-    //
-    // }, []);
 
     useEffect(() => {
         Axios.get(resolveAPIEndpoint("profiles/1") ).then(response => setData(response));
@@ -72,18 +78,24 @@ export default function Profile() {
     return (
         <Card className={classes.card}>
           <CardActionArea>
-            <CardMedia
-              className={classes.media}
-            //   image="/static/images/cards/contemplative-reptile.jpg"
-              title="Contemplative Reptile"
-            />
+          <CardHeader
+            avatar={
+             <Avatar aria-label="wagster" className={classes.avatar}>
+               W
+             </Avatar>
+            }
+          />
+          <CardMedia
+            className={classes.media}
+            image="/static/images/cards/contemplative-reptile.jpg"
+            title="Dog"
+          />
             <CardContent>
               <Typography gutterBottom variant="h5" component="h2">
                 {data.data.dog_name}
-
               </Typography>
               <Typography variant="body2" color="textSecondary" component="p">
-                Profile.biography
+                {data.data.biography}
               </Typography>
             </CardContent>
           </CardActionArea>
