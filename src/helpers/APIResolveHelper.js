fc3f8e1e1d93904120d2da467b905948cc8ffd56
Ben Zeng <benzeng94@me.com>
2019-11-04 12:50:32 +0000

    updated api helper env variables

--- src/helpers/APIResolveHelper.js
+++ src/helpers/APIResolveHelper.js
@@ -25,10 +25,9 @@
 // })(this);
 
 export function resolveAPIEndpoint(endpoint) {
-    if (process.env.NODE_ENV === 'development') {
-        return `http://localhost:3000/api/v1/${endpoint}`
-    }
-    if (process.env.NODE_ENV === 'production') {
-        return `https://api-wagster.herokuapp.com/api/v1/${endpoint}`
-    }
+    if (process.env.REACT_APP_STAGE === 'dev')
+        return `http://localhost:3000/api/v1/${endpoint}`;
+    else
+        return `https://api-wagster.herokuapp.com/api/v1/${endpoint}`;
+
 }
