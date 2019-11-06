b62ba2e9340a7dad0b916c8aa7ae0b7dc583b68e
ilimeza <ilianameza530@gmail.com>
2019-11-06 08:47:08 +0000

    Persist global state with localStorage

--- src/helpers/GlobalState.js
+++ src/helpers/GlobalState.js
@@ -1,4 +1,6 @@
-import { createGlobalState } from 'react-hooks-global-state';
+import React from 'react';
+
+const LOCAL_STORAGE_KEY = "wagster_frontend_global_state";
 
 const initialState = {
     currentUser: {
@@ -8,4 +10,35 @@ const initialState = {
     }
 };
 
-export const { GlobalStateProvider, useGlobalState } = createGlobalState(initialState);
+const persistedGlobalState = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY) || "{}");
+
+// Note: This isn't a deep clone, so whole objects e.g. `currentUser`
+// are being overwritten when `persistedGlobalState` is spread
+const globalState = {
+    ...initialState,
+    ...persistedGlobalState
+};
+
+/**
+ * These functions are a workaround for persisting
+ * application global state between new browser
+ * page loads - without it, the global state (e.g.
+ * your logged in state) are lost when you refresh
+ * the page in your browser.
+ */
+export function GlobalStateProvider(props) {
+    return (
+        <>{props.children}</>
+    )
+}
+
+export function useGlobalState(key) {
+    const value = globalState[key];
+
+    const setter = (newValue) => {
+        globalState[key] = newValue;
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(globalState));
+    };
+    
+    return [value, setter];
+}
